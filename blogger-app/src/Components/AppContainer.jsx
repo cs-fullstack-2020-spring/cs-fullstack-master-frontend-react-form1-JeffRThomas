2539a12aeb946c6b6cf082c05376fc6040ae59a8
JeffRThomas <thomas.jeff@live.com>
2020-03-20 11:40:31 -0500

    Blog Component Wiring

--- blogger-app/src/Components/AppContainer.jsx
+++ blogger-app/src/Components/AppContainer.jsx
@@ -1,27 +1,34 @@
-import React, {Component} from 'react';
-import {Fragment} from 'react';
+import React, { Component } from 'react';
+import { Fragment } from 'react';
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import PostList from './PostList.jsx';
 import BlogPost from './BlogPost.jsx'
 
 class AppContainer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            BlogPosts:[],
+        this.state = {
+            BlogPosts:
+             [
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora ut, ipsum excepturi praesentium veritatis quos exercitationem numquam non soluta. Fugiat quos culpa architecto id quibusdam eaque illo cum assumenda provident.",
+
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, nemo enim cumque ea officia similique est asperiores excepturi maiores dicta! Eius quibusdam optio, blanditiis voluptatum fugit asperiores perferendis rem distinctio.",
+
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet neque corrupti, repudiandae non libero dignissimos in ipsum minus amet? Laboriosam iure sed id atque fugiat aspernatur nesciunt, iste dolor cum!"
+            ],
         };
     };
 
-    render(){
-        return(
+    render() {
+        return (
             <Fragment>
                 {/* Component Roll Call */}
                 {/* <p>AppContainer is here!</p> */}
                 {/* <Header/>
                 <Footer/>
-                <PostList/>
-                <BlogPost/> */}
+                <PostList/> */}
+                <BlogPost/>
             </Fragment>
         )
     };
--- blogger-app/src/Components/BlogPost.jsx
+++ blogger-app/src/Components/BlogPost.jsx
@@ -1,20 +1,50 @@
-import React, {Component} from 'react';
-import {Fragment} from 'react';
+import React, { Component } from 'react';
+import { Fragment } from 'react';
 
 
 class BlogPosts extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            BlogPosts:[],
+        this.state = {
+            BlogPosts: [],
         };
     };
 
-    render(){
-        return(
+    userTitle=()=>{
+        console.log("New Title Keystroke");
+    };
+
+    userPost=()=>{
+        console.log("New Post Keystroke");
+    };
+
+    userSubmitBlog=(event)=>{
+        event.preventDefault();
+        console.log("Blog Post Entered");
+    };
+
+
+    render() {
+        return (
             <Fragment>
                 {/* Component Roll Call */}
                 {/* <p>BlogPost is here!</p> */}
+                <form>
+                    <fieldset>
+                        <legend>Enter Your Post</legend>
+                        <div>
+                            <label htmlFor="blogtitle">Blog Title: </label>
+                            <input type="text" id="blogtitle" onChange={this.userTitle} ></input>
+                        </div>
+
+                        <div>
+                            <label htmlFor="blogpost">Blog Post: </label>
+                            <textarea type="text" id="blogpost" onChange={this.userPost} ></textarea>
+                        </div>
+
+                        <button onClick={this.userSubmitBlog}>Submit Blog</button>
+                    </fieldset>
+                </form>
             </Fragment>
         )
     };
